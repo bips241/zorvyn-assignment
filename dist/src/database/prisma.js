925ee507dc9b22b8fab442aa9d54b5154b5a703b
Biplab Mal <biplabmal626@gmail.com>
2026-04-02 12:52:02 +0530

    refactor: remove user and record modules along with related routes, services, and tests
    - Deleted records.service.js, users.routes.js, users.schema.js, users.service.js, server.js, jwt.js, http-error.js, authenticate.js, authorize.js, global-error-handler.js, not-found-handler.js, validate.js, and various test files.
    - This cleanup removes unused code and consolidates the application structure.

--- dist/src/database/prisma.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-// Shared Prisma client instance used across services to keep DB access consistent.
-const client_1 = require("@prisma/client");
-exports.prisma = new client_1.PrismaClient();
